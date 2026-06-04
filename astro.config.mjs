785cde655c918a00881bd74fac1bc4aaaa0b0651
César Martín Martín - Anguís <163894686+Cesarb222@users.noreply.github.com>
2026-06-04 21:28:59 +0200

    Change site URL in astro.config.mjs
    Updated site URL to GitHub Pages format.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static', // ya soporta endpoints, hybrid ya no existe
-    site: 'https://Cesarb222.github.io',
-    base: '/portfolio'
+     // Reemplaza con tu dominio de GitHub Pages
+  site: 'https://github.io', 
+  
+  // IMPORTANTE: Pon el nombre exacto de tu repositorio con barras diagonales
+  base: '/portfolio', 
 });
