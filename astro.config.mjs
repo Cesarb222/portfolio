9d064f4ace09d1bb520275d161eaa9e4366aaaee
Cesarb222 <bargas33cesar@gmail.com>
2026-06-04 21:10:52 +0200

    Preparar despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static', // ya soporta endpoints, hybrid ya no existe
-    site: 'https://github.com/Cesarb222',
+    site: 'https://Cesarb222.github.io',
     base: '/portfolio'
 });
